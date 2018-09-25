6a427682dc788b277c5e4f20289d6738f53f2a4f
jvcasillas <joseph.casillas@rutgers.edu>
2018-09-24 23:13:04 -0400

    add sticker for academicWriteR

--- scripts/academicWriteR.R
+++ scripts/academicWriteR.R
@@ -2,24 +2,26 @@
 source("./scripts/helpers.R")
 
 url <- "jvcasillas.com/academicWriteR"
+img <- png::readPNG("./img_helpers/typewritter_3.png")
+rast <- grid::rasterGrob(img, interpolate = T)
+font_add_google("Metrophobic", "Metrophobic")
+myFont1 <- "Metrophobic"
 
-
-data <- tribble(
-  ~'x', ~'y',
-    0,    0)
-
-p <- data %>%
+p <-
+  tibble(x = rnorm(100), y = rnorm(100)) %>%
   ggplot(., aes(x = x, y = y)) +
-    geom_point(pch = 21, size = 20, stroke = 1, fill = 'white', alpha = 0.5) +
+    geom_point(alpha = 0) +
+    annotation_custom(rast, ymin = -2.5, ymax = 2.5, xmin = -2.4) +
+    annotate(geom = 'text', x = 0, y = 0, label = "",
+             size = 5.75, color = "grey10", family = myFont1) +
+    coord_cartesian(xlim = c(-2.1, 2.2), ylim = c(-3.75, 2)) +
     theme_void() +
     theme_transparent()
 
 sticker(
-  p, package = "academicWriteR", p_color = 'yellow',
+  p, package = "academicWriteR", url = url, p_color = 'white',
   p_family = "Aller_Rg", p_size = 5, p_y = 1.4,
-  s_x = 1, s_y = 0.8, s_width = 1.5, s_height = 1.5,
-  h_fill = 'blue', h_color = 'black', u_color = 'yellow', u_size = 1.25,
-  spotlight = T, l_x = 1, l_y = 0.85, l_alpha = 0.7,
-  l_width = 2.05, l_height = 1.8,
+  s_x = 1, s_y = 0.65, s_width = 1.5, s_height = 1.3,
+  h_fill = 'blue', h_color = 'darkblue', u_color = 'white', u_size = 1.5,
   filename = "stickers/academicWriteR.png")
 
